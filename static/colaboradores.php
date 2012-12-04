041d674eb21c92d6807d410ea93469fc81720d26
Manuel Alejandro Moscoso Domínguez <manuel@manuel-HP-620.(none)>
2012-12-03 23:00:45 -0300

    Add statis files

---
+++ static/colaboradores.php
@@ -0,0 +1,20 @@
+<div id="wrapper-content-column-left">
+
+</div>
+<div id="wrapper-content-column-mid" class="content-color">
+	<div class="column-title dgreen"><div class="text">Empress Inscritas que participan del programa</div></div>
+	<div class="column-enterprise">
+	<?
+	$i=1;
+	for($i=1;$i < 20; $i++) {
+	?>
+	<div class="enterprise">
+		
+	</div>
+	<?
+	} 
+	?>
+	</div>
+</div>
+<div id="wrapper-content-column-right">
+</div>
---
+++ static/inscribete.php
@@ -0,0 +1,36 @@
+<div id="wrapper-content-column-left">
+</div>
+<div id="wrapper-content-column-mid" class="content-color">
+	<div onclick="navegar(this.id)" id="title-one" class="column-title-one lgreen"><div class="text">Alumno</div></div>
+	<div onclick="navegar(this.id)" id="title-two" class="column-title-two lgreen"><div class="text">Empresa</div></div>
+	<table>
+		<tbody>
+			<tr>
+				<td class="first-column"></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="button" value="Registrar"></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<div id="wrapper-content-column-right">
+</div>
