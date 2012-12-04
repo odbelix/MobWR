f04b9dc780af8ec011b71d0ad95449d20a268efd
Manuel Alejandro Moscoso Domínguez <manuel@manuel-HP-620.(none)>
2012-12-04 16:37:48 -0300

    Add static tipo

---
+++ static/tipo.php
@@ -0,0 +1,21 @@
+<?php
+include_once('../lib/dblib.php');
+?>
+		<div id="wrapper-content-column-1" class="content-color">
+			<div class="column-title dgreen"><div class="text">Pŕactica Obrera</div></div>
+			<div class="column-content">
+				<img src="gallery/22.jpg" width="202px" height="365px">	
+			</div>
+			<div class="column-content-text">
+				<?=getMessage(7)?>
+			</div>	
+		</div>
+		<div id="wrapper-content-column-2" class="content-color">
+			<div class="column-title dgreen"><div class="text">Pŕactica Profesional</div></div>
+			<div class="column-content">
+				<img src="gallery/20.jpg" width="202px" height="365px">	
+			</div>
+			<div class="column-content-text">
+				<?=getMessage(8)?>
+			</div>
+		</div>
